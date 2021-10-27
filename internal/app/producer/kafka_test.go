7d01b624e5b2afa207c745275e07c66aff60499a
lgalkina <liudmila.galkina@gmail.com>
2021-10-27 10:03:32 +0300

    Producer test

---
+++ internal/app/producer/kafka_test.go
@@ -0,0 +1,89 @@
+package producer
+
+import (
+	"context"
+	"errors"
+	"github.com/gammazero/workerpool"
+	"github.com/golang/mock/gomock"
+	"github.com/lgalkina/act-correction-api/internal/app/cleaner"
+	"github.com/lgalkina/act-correction-api/internal/app/updater"
+	"github.com/lgalkina/act-correction-api/internal/mocks"
+	"github.com/lgalkina/act-correction-api/internal/model"
+	"testing"
+	"time"
+)
+
+var testEvent = model.CorrectionEvent{
+	ID:     1,
+	Type:   model.Created,
+	Status: model.Deferred,
+	Entity: &model.Correction{ID: 1, Timestamp: time.Now(), UserID: 1, Object: "order1 description", Action: "update",
+		Data: &model.Data{OriginalData: "test11", RevisedData: "test12"}},
+}
+
+func TestRemove(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repo := mocks.NewMockEventRepo(ctrl)
+	sender := mocks.NewMockEventSender(ctrl)
+
+	sender.EXPECT().Send(&testEvent).Return(nil).Times(1)
+	repo.EXPECT().Remove(gomock.Eq([]uint64{testEvent.ID})).Return(nil).Times(1)
+
+	events := make(chan model.CorrectionEvent)
+
+	producer := NewKafkaProducer(
+		1,
+		sender,
+		events,
+		workerpool.New(1),
+		updater.NewDbUpdater(repo),
+		cleaner.NewDbCleaner(repo))
+
+	cancelCtx, cancelCtxFunc := createCtx()
+
+	producer.Start(cancelCtx)
+	events <- testEvent
+	time.Sleep(time.Second)
+
+	cancelCtxFunc()
+	producer.Close()
+}
+
+func TestUnlock(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repo := mocks.NewMockEventRepo(ctrl)
+	sender := mocks.NewMockEventSender(ctrl)
+
+	sender.EXPECT().Send(&testEvent).Return(errors.New("send error")).Times(1)
+	repo.EXPECT().Unlock(gomock.Eq([]uint64{testEvent.ID})).Return(nil).Times(1)
+
+	events := make(chan model.CorrectionEvent)
+
+	producer := NewKafkaProducer(
+		1,
+		sender,
+		events,
+		workerpool.New(1),
+		updater.NewDbUpdater(repo),
+		cleaner.NewDbCleaner(repo))
+
+	cancelCtx, cancelCtxFunc := createCtx()
+
+	producer.Start(cancelCtx)
+	events <- testEvent
+	time.Sleep(time.Second)
+
+	cancelCtxFunc()
+	producer.Close()
+}
+
+func createCtx() (context.Context, context.CancelFunc) {
+	ctx := context.Background()
+	return context.WithCancel(ctx)
+}
--- internal/app/retranslator/retranslator_test.go
+++ internal/app/retranslator/retranslator_test.go
@@ -131,13 +131,13 @@ func setEventsSend(eventsNum int, sender *mocks.MockEventSender, events []model.
 
 func setEventsRemove(eventsNum int, repo *mocks.MockEventRepo, events []model.CorrectionEvent, error interface{}) {
 	for i := 0; i < eventsNum; i++ {
-		repo.EXPECT().Remove([]uint64{events[i].ID}).Return(error).Times(1)
+		repo.EXPECT().Remove(gomock.Eq([]uint64{events[i].ID})).Return(error).Times(1)
 	}
 }
 
 func setEventsUnlock(eventsNum int, repo *mocks.MockEventRepo, events []model.CorrectionEvent, error interface{}) {
 	for i := 0; i < eventsNum; i++ {
-		repo.EXPECT().Unlock([]uint64{events[i].ID}).Return(error).Times(1)
+		repo.EXPECT().Unlock(gomock.Eq([]uint64{events[i].ID})).Return(error).Times(1)
 	}
 }
 
@@ -160,42 +160,42 @@ func createEvents() []model.CorrectionEvent {
 		{
 			ID:     1,
 			Type:   model.Created,
-			Status: model.Processed,
+			Status: model.Deferred,
 			Entity: &model.Correction{ID: 1, Timestamp: time.Now(), UserID: 1, Object: "order1 description", Action: "update",
 				Data: &model.Data{OriginalData: "test11", RevisedData: "test12"}},
 		},
 		{
 			ID:     2,
 			Type:   model.Created,
-			Status: model.Processed,
+			Status: model.Deferred,
 			Entity: &model.Correction{ID: 2, Timestamp: time.Now(), UserID: 2, Object: "order2 description", Action: "update",
 				Data: &model.Data{OriginalData: "test21", RevisedData: "test22"}},
 		},
 		{
 			ID:     3,
 			Type:   model.Created,
-			Status: model.Processed,
+			Status: model.Deferred,
 			Entity: &model.Correction{ID: 3, Timestamp: time.Now(), UserID: 3, Object: "order3 description", Action: "update",
 				Data: &model.Data{OriginalData: "test31", RevisedData: "test32"}},
 		},
 		{
 			ID:     4,
 			Type:   model.Created,
-			Status: model.Processed,
+			Status: model.Deferred,
 			Entity: &model.Correction{ID: 4, Timestamp: time.Now(), UserID: 4, Object: "order4 description", Action: "update",
 				Data: &model.Data{OriginalData: "test41", RevisedData: "test42"}},
 		},
 		{
 			ID:     5,
 			Type:   model.Created,
-			Status: model.Processed,
+			Status: model.Deferred,
 			Entity: &model.Correction{ID: 5, Timestamp: time.Now(), UserID: 5, Object: "order5 description", Action: "update",
 				Data: &model.Data{OriginalData: "test51", RevisedData: "test52"}},
 		},
 		{
 			ID:     6,
 			Type:   model.Created,
-			Status: model.Processed,
+			Status: model.Deferred,
 			Entity: &model.Correction{ID: 5, Timestamp: time.Now(), UserID: 6, Object: "order6 description", Action: "update",
 				Data: &model.Data{OriginalData: "test61", RevisedData: "test62"}},
 		},
